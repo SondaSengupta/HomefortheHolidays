358ee5edb852130050430212a518c5252f78bae9
SondaSengupta <sondasengupta@gmail.com>
2014-12-22 15:37:22 -0600

    console.log coods from given address

--- scripts/main.js
+++ scripts/main.js
@@ -13,24 +13,33 @@
         console.log(err);
       })
 
+    var geocoder = new google.maps.Geocoder();
+
+    vm.getAddress = function(){
+      var address = document.getElementById('address').value;
+      geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var addressCoords = results[0].geometry.location;
+      console.log(addressCoords);
+
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+    }
 
     navigator.geolocation.getCurrentPosition(function(location) {
-      var lat = location.coords.latitude,
-          lng = location.coords.longitude;
-          console.log(lat + "," + lng);
-
-      $scope.$apply(function(){
-        $scope.lat = lat;
-        $scope.lng = lng;
-      })
+      $scope.lat = location.coords.latitude,
+      $scope.lng = location.coords.longitude;
+      $scope.$apply();
     });
 
+    console.log($scope.lat);
 
-
-      $scope.map = {
-        center: { latitude: 36, longitude: -87 },
-        zoom: 5
-      }
+    $scope.map = {
+      center: { latitude: 36, longitude: -87 },
+      zoom: 5
+    }
 
       $scope.marker = {
         id: 0,
@@ -45,27 +54,27 @@
 
       $scope.markerList = vm.Marker;
 
-    vm.addNewMarker = function(){
+    vm.addNewMarker = function() {
     var url = "https://holidayhome.firebaseio.com/.json";
     $http.post(url, vm.newMarker)
-      .success(function(data){
+      .success(function(data) {
         vm.Marker[data.name] = vm.newMarker;
         vm.newMarker = null;
         console.log("it works!");
       })
-      .error(function(err){
+      .error(function(err) {
         console.log(err);
       })
     }
 
-     vm.deleteMarker = function(markerId){
+     vm.deleteMarker = function(markerId) {
       var url = "https://holidayhome.firebaseio.com/" + markerId + ".json";
       $http.delete(url)
-      .success(function(){
+      .success(function() {
         delete vm.Marker[markerId]
         console.log("it works!");
       })
-      .error(function(err){
+      .error(function(err) {
         console.log(err);
       })
     }
