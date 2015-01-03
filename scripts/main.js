ea285cd3afa42e976f8b58ada544ad6d5c4502e6
SondaSengupta <sondasengupta@gmail.com>
2015-01-03 00:09:26 -0600

    Add Delete and delete authorization function

--- scripts/main.js
+++ scripts/main.js
@@ -117,6 +117,41 @@
             console.log(err);
           });
       };
+
+      vm.editUsername = function() {
+        var ref = new Firebase('https://holidayhome.firebaseio.com');
+
+        if (ref.getAuth()) {
+          var email = ref.getAuth().password.email;
+          var endSlicePosition = email.indexOf("@");
+          var username = email.slice(0, endSlicePosition);
+          $("input#editsignature").val("Last edited by " + username).trigger("input");
+
+        } else {
+         $("input#editsignature").val("Last edited by by Anonymous").trigger("input");
+        }
+      }
+
+      vm.deleteEditMarker = function() {
+       var ref = new Firebase('https://holidayhome.firebaseio.com');
+      console.log(id);
+      console.log(ref.getAuth().password.email);
+      console.log(vm.newMarker.email);
+       if(ref.getAuth().password.email === vm.newMarker.email){
+        var url = "https://holidayhome.firebaseio.com/" + id + ".json";
+        $http.delete(url)
+        .success(function() {
+          $location.path('/map')
+          alert("Your marker has been deleted. You will be returned to the Maps Page.")
+        })
+        .error(function(err) {
+          console.log(err);
+        })
+        } else {
+        alert("You are not authorized to delete this marker.");
+       }
+     }
+
     })
 
     .controller("MapController", function($http, $scope) {
@@ -223,6 +258,7 @@
         var endSlicePosition = email.indexOf("@");
         var username = email.slice(0, endSlicePosition);
         $("input#signature").val("Created by " + username).trigger("input");
+        $("input#email").val(email).trigger("input");
       } else {
        $("input#signature").val("Created by Anonymous").trigger("input");
       }
